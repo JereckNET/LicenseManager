9d63c72b1ec9739ea2cf536b16daa9063db2ca37
Olivier PIRLET <olivier@jereck.net>
2020-10-09 14:16:21 +0200

    - Created Sample project (Issue #3)

---
+++ LicenseManager.Sample/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace JereckNET.LicenseManager.Sample {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
